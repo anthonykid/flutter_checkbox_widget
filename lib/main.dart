003a7ee704f68da355dff69940a00668da307a0f
anthonykid <dickya630@gmail.com>
2021-06-13 03:26:01 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: FirstScreen(), // Panggil FirstScreen di sini
+    );
+  }
+}
+
+class FirstScreen extends StatefulWidget {
+  @override
+  _FirstScreenState createState() => _FirstScreenState();
+}
+
+    class _FirstScreenState extends State<FirstScreen> {
+      bool agree = false;
+     
+      @override
+      Widget build(BuildContext context) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text('First Screen'),
+          ),
+          body: ListTile(
+            leading: Checkbox(
+              value: agree,
+              onChanged: (bool value) {
+                setState(() {
+                  agree = value;
+                });
+              },
+            ),
+            title: Text('Agree / Disagree'),
+          ),
+        );
+      }
+    }
